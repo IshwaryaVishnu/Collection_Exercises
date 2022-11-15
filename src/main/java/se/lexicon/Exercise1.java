84f34fbb59ba7d18fb9bcb8f2f69de30c973f668
Iswarya Devi Gomathi Sankar <ishu27.v@gmail.com>
2022-11-15 17:19:12 +0100

    Array List

--- src/main/java/se/lexicon/Exercise1.java
+++ src/main/java/se/lexicon/Exercise1.java
@@ -1,6 +1,16 @@
 package se.lexicon;
 import java.util.*;
 public class Exercise1 {
-
+    public static void main(String [] args){
+        ArrayList<String> week = new ArrayList<>();
+        week.add("Sunday");
+        week.add("Monday");
+        week.add("Tuesday");
+        week.add("wednesday");
+        week.add("Thursday");
+        week.add("Friday");
+        week.add("Saturday");
+        System.out.println("weeks:" + week);
+    }
 
 }
